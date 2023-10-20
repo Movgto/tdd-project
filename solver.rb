1de35dfc41d7c026c78da281a85673f833a0c6c8
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-20 16:30:07 +0430

    extra method removed

--- solver.rb
+++ solver.rb
@@ -12,9 +12,23 @@ class Solver
   def reverse(str)
     str.chars.reverse.join
   end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
+
 end
 
 solver = Solver.new
 
 p solver.factorial(5) # => 120
 p solver.reverse('hello')
+p solver.fizzbuzz(6)
