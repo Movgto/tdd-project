f00f13bac4d3e342b082f3adb32e32aa6a6923fc
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-20 16:10:50 +0430

    test added for the fizz buzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -26,4 +26,22 @@ describe 'reverse' do
   it 'the arguments is hello should to return olleh' do
     expect(solver.reverse('hello')).to eq('olleh')
   end
+  
+  describe 'fizzbuzz' do
+    it 'When N is divisible by 3, return "fizz"' do
+      expect(solver.fizzbuzz(6)).to eq('fizz')
+    end
+
+    it 'When N is divisible by 5, return "buzz"' do
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'When N is divisible by 3 and 5, return "fizzbuzz"' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'Any other case, return N as a string (e.g. say N is 7 then return "7")' do
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
